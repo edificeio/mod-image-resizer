66fe00fd10925dd16b52e19d170f81a821fadd41
Pierre-Benoît JOUBERT <pierre-benoit.joubert@edifice.io>
2026-03-18 15:54:12 +0100

    chore: #ENABLING-659, add unsecure ssl option

--- src/main/java/fr/wseduc/resizer/ImageResizer.java
+++ src/main/java/fr/wseduc/resizer/ImageResizer.java
@@ -74,9 +74,10 @@ public class ImageResizer extends BusModBase implements Handler<Message<JsonObje
 			String region = s3.getString("region");
 			String bucket = s3.getString("bucket");
 			String ssec = s3.getString("ssec", null);
+			boolean trustAll = s3.getBoolean("trustAll", false);
 			if (uri != null && accessKey != null && secretKey != null && region != null && bucket != null) {
 				try {
-					fileAccessProviders.put("s3", new S3Access(vertx, new URI(uri), accessKey, secretKey, region, bucket, ssec));
+					fileAccessProviders.put("s3", new S3Access(vertx, new URI(uri), accessKey, secretKey, region, bucket, ssec, trustAll));
 				} catch (URISyntaxException e) {
 					logger.error("Invalid s3 uri.", e);
 				}
